c8075504311484041bc4c7f3dfe04f88fe9098db
Ezekiel <bazzdude007@gmail.com>
2025-11-23 17:37:09 +0000

    comments added

--- factorial.cpp
+++ factorial.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
-//Note that this only calculates as high as 170! which the biggest a c++
-//data type can contain before overflowing.
-//An external function or custom implementation is required to 
-//contain higher factorial results
+// Note that this only calculates as high as 170! which the biggest a c++
+// data type can contain before overflowing.
+// An external function or custom implementation is required to 
+// contain higher factorial results
 long double factorial(int n) {
 
+    // The base case terminates the function when n = 1 or n = 0,
+    // just like 0! = 1 and 1! = 1
     if (n == 0 || n == 1) {
         return 1;
     }
+
+    // The recursive case calculates and returns the value for n * (n - 1)!, 
+    // as is the case for calculating factorials
     return (n * factorial(n - 1));
 }
 
